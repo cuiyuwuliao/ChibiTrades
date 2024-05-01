bc00973d0f2d3d4aeaf1e53266129c7e79ac4b12
Aethergo1999 <aethergo1999@gmail.com>
2024-05-01 09:33:39 +0800

    0501

--- Data_preprocess.py
+++ Data_preprocess.py
@@ -4,7 +4,7 @@ import multiple as mtp
 import time
 import random
 import DataSelect
-from datetime import datetime;
+from datetime import datetime
 
 # 项目目录
 proj_dir = os.path.dirname(os.path.abspath(__file__))
@@ -49,7 +49,7 @@ base_time_fix_oldest = base_time_oldest + 36*24*3600 + 1 #增加36天
 
 # 随机生成时间戳
 timestamp1 = [random.randint(base_time_fix_latest-15*24*3600, base_time_fix_latest) for _ in range(5000)] #注意此处用秒为单位 而不是毫秒
-timestamp2 = [random.randint(base_time_fix_oldest, base_time_fix_latest) for _ in range(15000)]
+timestamp2 = [random.randint(base_time_fix_oldest, base_time_fix_latest) for _ in range(17000)]
 timestamps = timestamp1+timestamp2
 
 # 定义全局变量
